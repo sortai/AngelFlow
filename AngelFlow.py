6c3be3bf4481e7dd5debfaac0b0373209d094990
sortai <32301806+sortai@users.noreply.github.com>
2019-11-14 17:42:47 +0100

    Update AngelFlow.py

--- AngelFlow.py
+++ AngelFlow.py
@@ -6,17 +6,17 @@ class layer:
 
 class mlayer(layer):
     """Matrix layer."""
-    def __init__(self, io = None, imat = None):
-        if imat is None:
+    def __init__(self, io = None, mat = None):
+        if mat is None:
             if io is None:
                 #shape is assumed to be (1, 1)
                 self.mat = np.mat("1+0j")
                 self.vec = np.mat("0j")
             else:
                 #truncated identity
-                self.mat = np.mat("; ".join("0+0j "*min(i,io[0])+"1+0j"*int(i<io[0])+" 0+0j"*max(io[0]-1-i,0) for i in range(io[1])))
+                self.mat = np.mat(np.eye(io[-1],io[0], dtype=np.complex_))
         else:
-            self.mat = np.mat(imat)
+            self.mat = np.mat(mat)
                 
     @property
     def io(self):
